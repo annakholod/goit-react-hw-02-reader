11d83b843df0514c972e5372cbb4751d1bb3f25a
Anna Kholod <uashop89@gmail.com>
2019-09-28 11:32:34 +0300

    Updates

---
+++ precache-manifest.936d981baf45ca1b6fb82545ba1ec71a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba132f7df2158361ac7dd7132cd629d7",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "a0d6fcf1fe341a44d01f",
+    "url": "/goit-react-hw-02-reader/static/css/main.6b003aed.chunk.css"
+  },
+  {
+    "revision": "533b8e7e9ca878a54c42",
+    "url": "/goit-react-hw-02-reader/static/js/2.78b03821.chunk.js"
+  },
+  {
+    "revision": "a0d6fcf1fe341a44d01f",
+    "url": "/goit-react-hw-02-reader/static/js/main.b76261fd.chunk.js"
+  },
+  {
+    "revision": "00f7643cf5fd8a235fee",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.85f11a9b.js"
+  }
+]);
